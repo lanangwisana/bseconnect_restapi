dc43151902fc265369010c8315ad96908453141f
lanangwisana <lanangwisana@gmail.com>
2024-05-31 23:31:59 +0700

    bseconnect v.5

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/index', function () {
-    return view('index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/presensi', function(){
+    return view('presensi');
 });
